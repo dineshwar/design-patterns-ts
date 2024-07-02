1d57be68e76c7483e4dda78b636608a3f86ef477
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-07-02 23:24:21 +0530

    Update simple factory

--- creational/simple_factory.ts
+++ creational/simple_factory.ts
@@ -1,3 +1,21 @@
-interface Book {
-  
+interface Door {
+  getWidth(): number;
+  getHeight(): number;
+}
+
+class WoodenDoor implements Door {
+  width: number;
+  height: number;
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getWidth() {
+    return this.width;
+  }
+
+  getHeight() {
+    return this.height;
+  }
 }
